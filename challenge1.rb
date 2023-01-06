507d3318fb783102761695b35ed3bfcd4a1ab0fd
Sean Cowans <zedesia@yahoo.com>
2023-01-06 11:16:53 -0700

    Add challenge1

---
+++ challenge1.rb
@@ -0,0 +1,10 @@
+#challenge1
+#create array
+#use .each to reference each string
+#use .length to check string legnth
+#equal length to 4
+#put the name
+names = ["John","Jacob","Jingleheimer","Schmidt"]
+names.each do |name|
+  puts name if name.length == 4
+end
