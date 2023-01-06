6ce9d83e9d399f70149bec0321f189990dc769e5
Sean Cowans <zedesia@yahoo.com>
2023-01-06 11:20:16 -0700

    Add challenge2

---
+++ challenge2.rb
@@ -0,0 +1,10 @@
+#challenge2
+#create array
+#use .each to reference each string
+#use .downcase to make lowercase
+#put the array
+
+name = ["JoJo","JimJim","BonBon"]
+name.each do |edit|
+  puts edit.downcase
+end
